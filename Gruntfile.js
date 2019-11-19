0b265af9fdeb293f977736545773b90c6e5b2dc3
Spencer Lee <37911329+PouncingBox@users.noreply.github.com>
2019-11-19 00:26:00 -0500

    Sprite tweaks; mechanics change; polish
    Tweaked sprites to allow animations that only loop once. Made the game reward success instead of punishing failure. Reverted mechanics such that player commits to their move. Sped up the game. Added fancy background. Exploding dog.

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,5 +84,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Run default tasks
-    grunt.registerTask('default', ['jshint', 'clean', /*'imagemin',*/ 'concat', 'terser']);
+    grunt.registerTask('default', ['jshint', 'clean', 'imagemin', 'concat', 'terser']);
 };
